4f784980146547df6d3785eb15792f6bf1044716
BRADLEY WITTENBROOK <bradley@BRADLEYs-MacBook-Pro.local>
2015-02-18 02:15:04 -0500

    Added fancy daemon message formatting.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 service mysql start
 service rabbitmq-server start 
+
+. /lib/lsb/init-functions
+
+# Configure Identity Service 
+log_daemon_msg "Configuring Identity Service"
+mysql -u root -Bse "CREATE DATABASE keystone;"
+mysql -u root -Bse "GRANT ALL PRIVILEGES ON keystone.* \
+	TO 'keystone'@'localhost' \
+  	IDENTIFIED BY '$KEYSTONE_DBPASS';"
+mysql -u root -Bse "GRANT ALL PRIVILEGES ON keystone.* \
+	TO 'keystone'@'%' \
+  	IDENTIFIED BY '$KEYSTONE_DBPASS';"
+log_end_msg 0
+
+
